ce352bd904ffb4b2a1dba43b2b64a5ad96ef4330
Stroescu Matei-Stefan <cptmatix@gmail.com>
2026-07-13 14:56:25 +0300

    All 35 opcodes done

--- src/chip8.c
+++ src/chip8.c
@@ -1,8 +1,7 @@
 #include "chip8.h"
 #include <stdint.h>
 #include <string.h> 
-#include <stdio.h>
-
+#include <stdlib.h>
 static const uint8_t chip8_fontset[80] = {
      0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
     0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -61,21 +60,26 @@ void chip8_cycle(chip8_t *chip8)
             break;
         case 0x2:
         {
-            chip8->sp++;
-            chip8->stack[chip8->sp] = chip8->pc ;
+            chip8->stack[++chip8->sp] = chip8->pc + 2;
             chip8->pc = (opcode & 0x0FFF);
             break;
         }
         case 0x3:
             if(chip8->v[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
+                chip8->pc += 4;
+            else 
                 chip8->pc += 2;
             break;
         case 0x4:
             if(chip8->v[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
+                chip8->pc += 4;
+            else
                 chip8->pc += 2;
             break;
         case 0x5:
             if(chip8->v[(opcode & 0x0F00) >> 8] == chip8->v[(opcode & 0x00F0) >> 4])
+                chip8->pc += 4;
+            else
                 chip8->pc += 2;
             break;
         case 0x6:
@@ -87,9 +91,69 @@ void chip8_cycle(chip8_t *chip8)
             chip8->pc += 2;
             break;
         case 0x8:
+        {
+            uint8_t x = ((opcode & 0x0F00) >> 8);
+            uint8_t y = ((opcode & 0x00F0) >> 4);
+            switch((opcode & 0x000F))
+            {
+                case 0x0:
+                    chip8->v[x] = chip8->v[y];
+                    break;
+                case 0x1:
+                    chip8->v[x] = (chip8->v[x] | chip8->v[y]);
+                    break;
+                case 0x2:
+                    chip8->v[x] = (chip8->v[x] & chip8->v[y]);
+                    break;
+                case 0x3:
+                    chip8->v[x] = (chip8->v[x] ^ chip8->v[y]);
+                    break;
+                case 0x4:
+                    if(chip8->v[x] + chip8->v[y] > 255)
+                        chip8->v[0xF] = 1;
+                    else
+                        chip8->v[0xF] = 0;
+                    chip8->v[x] = (chip8->v[x] + chip8->v[y]) & 0xFF;
+                    break;
+                case 0x5:
+                    if(chip8->v[x] >= chip8->v[y])
+                        chip8->v[0xF] = 1;
+                    else 
+                        chip8->v[0xF] = 0;
+                    chip8->v[x] = chip8->v[x] - chip8->v[y];
+                    break;
+                case 0x6:
+                    if((chip8->v[x] & 1) == 0x1)
+                       chip8->v[0xF] = 1;
+                    else 
+                        chip8->v[0xF] = 0;
+                    chip8->v[x] /= 2;
+                    break;
+                case 0x7:
+                    if(chip8->v[y] >= chip8->v[x])
+                        chip8->v[0xF] = 1;
+                    else 
+                        chip8->v[0xF] = 0;
+                    chip8->v[x] = chip8->v[y] - chip8->v[x];
+                    break;
+                case 0xE:
+                    if(((chip8->v[x] & (1 << 7)) >> 7) == 0x1)
+                       chip8->v[0xF] = 1;
+                    else 
+                        chip8->v[0xF] = 0;
+                    chip8->v[x] *= 2;
+                    break;
+                default:
+                    break;
+            }
+            chip8->pc += 2;
+
+        }
             break;
         case 0x9:
-        if(chip8->v[(opcode & 0x0F00) >> 8] != chip8->v[(opcode & 0x00F0) >> 4])
+            if(chip8->v[(opcode & 0x0F00) >> 8] != chip8->v[(opcode & 0x00F0) >> 4])
+                chip8->pc += 4;
+            else
                 chip8->pc += 2;
             break;
         case 0xA:
@@ -97,8 +161,11 @@ void chip8_cycle(chip8_t *chip8)
             chip8->pc += 2;
             break;
         case 0xB:
+            chip8->pc = ((opcode & 0x0FFF) + chip8->v[0]);
             break;
         case 0xC:
+            chip8->v[(opcode & 0x0F00) >> 8] = (rand() % 256) & (opcode & 0x00FF);  
+            chip8->pc += 2;
             break;
         case 0xD:
         {
@@ -106,7 +173,6 @@ void chip8_cycle(chip8_t *chip8)
             uint8_t Y = (opcode & 0x00F0) >> 4;
             uint8_t n = opcode & 0x000F;
             chip8->v[0xF] = 0;
-            printf("Draw: X=%d Y=%d n=%d I=0x%03X VX=%d VY=%d\n", X, Y, n, chip8->I, chip8->v[X], chip8->v[Y]);
             for(uint8_t linie = 0; linie < n; linie++)
             {
                 uint8_t curr = chip8->memory[chip8->I + linie];
@@ -127,9 +193,79 @@ void chip8_cycle(chip8_t *chip8)
                 break;
         }
         case 0xE:
+        {
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            if((opcode & 0x00FF) == 0x9E)
+            {
+                if(chip8->keys[chip8->v[x]] == 1)
+                    chip8->pc += 4;
+                else
+                    chip8->pc += 2;
+            }
+            else if((opcode & 0x00FF) == 0xA1)
+            {
+                if(chip8->keys[chip8->v[x]] == 0)
+                    chip8->pc += 4;
+                else
+                    chip8->pc += 2;
+            }
             break;
+        }
         case 0xF:
+        {
+            uint8_t x = (opcode & 0x0F00) >> 8;
+            switch(opcode & 0x00FF)
+            {
+                case 0x07:
+                    chip8->v[x] = chip8->timer_delay;
+                    chip8->pc += 2; 
+                    break;
+                case 0x15:
+                    chip8->timer_delay = chip8->v[x];
+                    chip8->pc += 2; 
+                    break;
+                case 0x18:
+                    chip8->timer_sound = chip8->v[x];
+                    chip8->pc += 2; 
+                    break;
+                case 0x1E:
+                    chip8->I += chip8->v[x];
+                    chip8->pc += 2; 
+                    break;
+                case 0x29:
+                    chip8->I = chip8->v[x] * 5;
+                    chip8->pc += 2; 
+                    break;
+                case 0x33:
+                    chip8->memory[chip8->I] = chip8->v[x] / 100;
+                    chip8->memory[chip8->I + 1] = (chip8->v[x] / 10) % 10;
+                    chip8->memory[chip8->I + 2] = (chip8->v[x]) % 10;
+                    chip8->pc += 2; 
+                    break;
+                case 0x55:
+                    for(int i = 0; i <= x; i++)
+                        chip8->memory[chip8->I + i] = chip8->v[i]; 
+                    chip8->pc += 2; 
+                    break;
+                case 0x65:
+                    for(int i = 0; i <= x; i++)
+                         chip8->v[i] = chip8->memory[chip8->I + i]; 
+                    chip8->pc += 2; 
+                    break;
+                case 0x0A:
+                    for(int i = 0; i < 16; i++)
+                        if(chip8->keys[i] == 1)
+                        {
+                            chip8->v[x] = i;
+                            chip8->pc += 2; 
+                            break;
+                        }
+                    break;
+                default:
+                    break;
+            }
             break;
+        }
         default:
             break;
     }
